9c8d75238d5c872786c4b78cce1041655e3f2a3a
Mauro Rossoni <junior.rossonibn@gmail.com>
2022-01-14 16:46:11 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,34 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
 
-    return(
-        <Pagination />
-        );
+    return (
+        <>
+            <Pagination />
 
-    }
-    
-    export default Listing;
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-4 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />                           
+                    </div>
+                    <div className="col-sm-4 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />                           
+                    </div>
+                    <div className="col-sm-4 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />                           
+                    </div>
+                    <div className="col-sm-4 col-lg-4 col-x1-3 mb-3">
+                        <MovieCard />                           
+                    </div>
+                </div>
+            </div>
+
+
+
+        </>
+    );
+
+}
+
+export default Listing;

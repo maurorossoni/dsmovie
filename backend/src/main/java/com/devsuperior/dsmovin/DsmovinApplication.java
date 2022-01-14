378e0e5f6cb10031eed9ea710d87777342323475
Mauro Rossoni <junior.rossonibn@gmail.com>
2022-01-14 11:51:04 -0300

    project creat

---
+++ backend/src/main/java/com/devsuperior/dsmovin/DsmovinApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovinApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmovin/DsmovinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
